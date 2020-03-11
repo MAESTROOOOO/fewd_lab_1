f8da86f593f12596c74ef531e0fe6edbbf6a6469
MAESTROOOOO <maestrovirtuoso45@gmail.com>
2020-03-11 15:59:09 +0000

    javascript

---
+++ fewb_lab_1_doc.js
@@ -0,0 +1,22 @@
+var firstName = 'Jean-Christophe';
+var favouriteColor = "Blue";
+var favouriteFood = 'Pizza';
+var city = 'Paris';
+
+//prints the values of the different variables
+console.log(firstName);
+console.log(favouriteColor);
+console.log(favouriteFood);
+console.log(city);
+
+var phrase = 'A rainbow is formed by the sunlight. Shining through rain drops sosmall. The rain bends the light from the sunshine, and colors the sky for us all.';
+var endingPhrase = 'is what we see. Rainbow colours, are made with light bendingyou see!';
+var one = 'Red';
+var two = 'orange';
+var three = 'yellow'
+var four = 'green';
+var five = 'blue';
+var six = 'indigo';
+var seven = 'violet';
+
+console.log(phrase + one + ", " + two + ", " + three + ", " + four + ", " + five + ", " + six + ", " + seven + " " + endingPhrase);
